7fa66c077857a4461dac1ffe75bd65b41f1c8f90
AhmedAfzal2089 <ameafzal@gmail.com>
2025-12-14 18:18:56 +0500

    intro to mixins

--- api/views.py
+++ api/views.py
@@ -12,6 +12,7 @@ from rest_framework.views import APIView
 from employees.models import Employee
 from django.http import Http404
 from api import serializers
+from rest_framework import mixins,generics
 # Create your views here.
 
 @api_view(['GET','POST'])
@@ -53,37 +54,52 @@ def studentDetailView(request,pk):
         
         
         
-class Employees(APIView):
-    def get(self,request):
-        employees = Employee.objects.all()
-        serializers = EmployeeSerializer(employees, many=True)
-        return Response(serializers.data,status=status.HTTP_200_OK)
+# class Employees(APIView):
+#     def get(self,request):
+#         employees = Employee.objects.all()
+#         serializers = EmployeeSerializer(employees, many=True)
+#         return Response(serializers.data,status=status.HTTP_200_OK)
 
-    def post(self, request):
-        serializer = EmployeeSerializer(data = request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+#     def post(self, request):
+#         serializer = EmployeeSerializer(data = request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
     
-class EmployeeDetail(APIView):
-    def get_object(self,pk):
-        try:
-            return Employee.objects.get(pk=pk)
-        except Employee.DoesNotExist:
-            raise Http404
-    def get(self,request,pk):
-        employee = self.get_object(pk)
-        serializer = EmployeeSerializer(employee)
-        return Response(serializer.data,status = status.HTTP_200_OK)
-    def put(self,request,pk):
-        employee = self.get_object(pk)
-        serializer = EmployeeSerializer(employee,data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data,status=status.HTTP_200_OK )
-        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
-    def delete(self,request,pk):
-        emoloyee= self.get_object(pk)
-        emoloyee.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+# class EmployeeDetail(APIView):
+#     def get_object(self,pk):
+#         try:
+#             return Employee.objects.get(pk=pk)
+#         except Employee.DoesNotExist:
+#             raise Http404
+#     def get(self,request,pk):
+#         employee = self.get_object(pk)
+#         serializer = EmployeeSerializer(employee)
+#         return Response(serializer.data,status = status.HTTP_200_OK)
+#     def put(self,request,pk):
+#         employee = self.get_object(pk)
+#         serializer = EmployeeSerializer(employee,data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data,status=status.HTTP_200_OK )
+#         return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+#     def delete(self,request,pk):
+#         emoloyee= self.get_object(pk)
+#         emoloyee.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class Employees(mixins.ListModelMixin,mixins.CreateModelMixin,generics.GenericAPIView):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
+
+    def get(self,request):
+        return self.list(request)
+    
+    def post(self,request):
+        return self.create(request)
+
+
+class EmployeeDetail(generics.GenericAPIView):
+    pass
