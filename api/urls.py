c3c480cd9e4c8152eefe5994b5f4e4447e006f16
AhmedAfzal2089 <ameafzal@gmail.com>
2025-12-14 16:32:25 +0500

    add get single employee

--- api/urls.py
+++ api/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('students/',views.studentsView),
     path('students/<int:pk>/',views.studentDetailView),
     
-    path('employees/',views.Employees.as_view())
+    path('employees/',views.Employees.as_view()),
+    path('employees/<int:pk>/',views.EmployeeDetail.as_view())
 ]
